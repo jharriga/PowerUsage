1e9ca16955d2a716f906a6d1fc95c4c60198fff7
Keith Valin <kvalin@redhat.com>
2024-02-07 11:26:32 -0500

    fix pbench-scripts: Correct path to python script

--- pbench_scripts/start.sh
+++ pbench_scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPT_DIR=$(dirname -- "$0")
-python3 $SCRIPT_DIR/json_PRO3X.py --ip 10.27.242.2 --interval 5 --outlet 3 --output $1/result.json &
+python3 $SCRIPT_DIR/../json_PRO3X.py --ip 10.27.242.2 --interval 5 --outlet 3 --output $1/result.json &
 echo "$!" > $1/power.pid
