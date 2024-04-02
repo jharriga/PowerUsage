e1e068a8e19390aac6e8f27414f5cc285a19e9d2
Keith Valin <kvalin@redhat.com>
2024-04-02 14:04:49 -0400

    Add Redfish username and password to vars file

--- pbench_scripts/start.sh
+++ pbench_scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR=$(dirname -- "$0")
 source $SCRIPT_DIR/vars.sh
-python3 $SCRIPT_DIR/../json_PRO3X.py --ip $IP  --interval $INTERVAL --outlet $OUTLET --output $1/result.json &
+python3 $SCRIPT_DIR/../json_PRO3X.py --ip $IP  --interval $INTERVAL --outlet $OUTLET --user $RF_USER --password $RF_PASSWORD --output $1/result.json &
 
 echo "$!" > $1/power.pid
