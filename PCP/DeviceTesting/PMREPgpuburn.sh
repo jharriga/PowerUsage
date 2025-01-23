58ed1adc1fcea0778e332b5b92a84ff139767095
John Harrigan <jharriga@redhat.com>
2025-01-23 15:26:27 -0500

    Update PMREPgpuburn.sh

--- PCP/DeviceTesting/PMREPgpuburn.sh
+++ PCP/DeviceTesting/PMREPgpuburn.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-# Run with existing Power Cap
-    
+# Script which attempts to quatify GPU Power Efficiency by varying
+# the GPU Frequency while the 'gpu_burn stress test. 
+# NOTE: the script doesnot modify the existing Power Cap
+###################################################################
+
+# Configure GPU Frequencies for the test runs
+# MIN and MAX were hardcoded for Nvidia A100
+min_freq=210
+max_freq=1410
+multiplier=2 
+
 delay=15        # Fixed DELAY between INNER-LOOP runs
 runtime=30      # runtime duration for each gpu-burn run
 sampletime=3    # sample time for PMREP Metrics
@@ -11,19 +20,20 @@ workload="${executable} ${runtime}"
 echo "Workload: ${workload}"
 
 # Verify workload is avalaible on the system
-if 
 if [ ! -x "$executable" ]; then
   echo "File ${executable} is not found. Exiting"
   exit 1
 fi
 
 # OUTER Loop - vary GPU Frequency
-freq_array=
-for multiplier in {1..4}; do
-    this_freq=$((duration*multiplier))   *REDO*
-    echo "GPU Freq for these runs: ${this_freq}"
+# Initialize vars for first loop
+loop_ctr=1
+this_freq=$(( min_freq*loop_ctr ))
+
+while [ $this_freq .lt. $max_freq ]; do
+    echo "GPU Freq for this set of runs: ${this_freq}"
     # Apply GPU Frequency
-    nvidia-smi -lgc $this_freq,$this_freq
+    nvidia-smi -lgc $min_freq,$this_freq
 
     fname="${this_freq}sec.csv"
 
@@ -41,9 +51,9 @@ for multiplier in {1..4}; do
     for sample_ctr in {1..5}; do
         echo -n "${sample_ctr}, "
         sleep $delay
-##        $workload &> /dev/null
+##        $workload &> /dev/null     # Record GFLOPS for the run
     done
-# Terminate PMREP. Flush buffers by sendng SIGUSR1 signal 
+    # Terminate PMREP. Flush buffers by sendng SIGUSR1 signal 
     kill -SIGUSR1 ${pmrepPID}
     echo; echo "------------------"
 
@@ -53,4 +63,8 @@ for multiplier in {1..4}; do
         echo "Failed to create $fname, exiting"
         exit 1
     fi
+    
+    # Initialize vars for next loop. Bail if you exceed MAX_FREQ
+    ((loop_ctr++))
+    this_freq=$(( min_freq*loop_ctr )) 
 done
